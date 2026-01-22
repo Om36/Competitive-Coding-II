5a14bd48fc3eefcb7bdd26c1bd2e1056d69bc4db
Om Singh <153166238+Om36@users.noreply.github.com>
2026-01-22 13:49:02 +0530

    Add Product of Array Except Self solution
    Implement a solution to calculate the product of an array except self using a nested loop approach.

---
+++ Product of an Array Except Self.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+class Solution {
+public:
+    vector<int> productExceptSelf(vector<int>& nums) {
+        int n = nums.size();
+        vector<int> res(n, 1);
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                if (i != j) {
+                    res[i] *= nums[j];
+                }
+            }
+        }
+
+        return res;
+    }
+};
+
+int main() {
+    Solution sol;
+    
+    vector<int> nums = {1, 2, 3, 4};
+    vector<int> result = sol.productExceptSelf(nums);
+
+    for (int x : result) {
+        cout << x << " ";
+    }
+
+    return 0;
+}
